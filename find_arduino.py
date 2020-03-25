d7f237c41e5e6800ad69f39747b0bbaa81f69ae7
ianhi <ianhuntisaak@gmail.com>
2020-03-25 14:37:43 -0400

    create find_arduino.py
    This is the file where the actual functionality will go.

---
+++ find_arduino.py
@@ -0,0 +1,28 @@
+from boards import boards_dict
+from serial.tools import list_ports
+import re
+
+def check_ports():
+    not_arduinos = []
+    arduinos = []
+    for connection in list_ports.comports():
+        port,hwid,desc = connection
+        vid_pid = re.search(r"(?<=VID\:PID\=)[0-9|A-Z|a-z]{4}\:[0-9|A-Z|a-z]{4}", desc)
+        vid_pid = None if vid_pid is None else vid_pid.group()
+        print(vid_pid)
+        if vid_pid is None:
+            not_arduinos.append(connection)
+        else:
+            try:
+                board = boards_dict[vid_pid]
+                arduinos.append((board,connection))
+            except KeyError:
+                not_arduinos.append(connection)
+    return arduinos, not_arduinos
+
+if __name__ == "__main__":
+    arduinos, not_arduinos = check_ports()
+    for ard in arduinos:
+        print(ard)
+    for nard in not_arduinos:
+        print(nard)
